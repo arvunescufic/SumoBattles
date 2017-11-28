958f6dc80e7198e39e855edda56e3e896b0c9f06
AlinPOPOVICI <popovici.alin@gmail.com>
2017-11-28 11:37:57 +0200

    Al 4 lea commit

--- FIC2017/VideoTool.cpp
+++ FIC2017/VideoTool.cpp
@@ -3,11 +3,17 @@
 #include <iostream>
 //#include <opencv2\highgui.h>
 #include "opencv2/highgui/highgui.hpp"
-//#include <opencv2\cv.h>
-#include "opencv2/opencv.hpp"
-
+#include <opencv2\cv.h>
+//#include "opencv2/opencv.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
 using namespace std;
 using namespace cv;
+
+
 //initial min and max HSV filter values.
 //these will be changed using trackbars
 int H_MIN = 161;
@@ -37,6 +43,21 @@ const std::string windowName2 = "Thresholded Image";
 const std::string windowName3 = "After Morphological Operations";
 const std::string trackbarWindowName = "Trackbars";
 
+void move(int sock, char *buf) {
+	int n;
+	char aux[5];
+	while(buf[0] != '\0') {	
+ 	 	/* Send message to the server */
+		sprintf(aux,"%c",buf[0]);
+   		n = write(sock, aux, 1);
+   		strcpy(buf,buf+1);
+  	 	if (n < 0) {
+      			perror("ERROR writing to socket");
+   	        	exit(1);
+   	 	}
+		sleep(1); // delay in miliseconds
+	}
+}
 
 void on_mouse(int e, int x, int y, int d, void *ptr)
 {
@@ -183,7 +204,42 @@ void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed) {
 }
 int main(int argc, char* argv[])
 {
-
+	int sockfd, portno;
+   struct sockaddr_in serv_addr;
+   struct hostent *server;
+   char buffer[256];
+   if (argc < 3) {
+      fprintf(stderr,"usage %s hostname port\n", argv[0]);
+      exit(0);
+   }
+   portno = atoi(argv[2]);
+   /* Create a socket point */
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   if (sockfd < 0) {
+      perror("ERROR opening socket");
+      exit(1);
+   }
+   server = gethostbyname(argv[1]);
+   if (server == NULL) {
+      fprintf(stderr,"ERROR, no such host\n");
+      exit(0);
+   }
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+   serv_addr.sin_port = htons(portno);
+   /* Now connect to the server */
+   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR connecting");
+      exit(1);
+   }
+   /* Now ask for a message from the user, this message
+      * will be read by server
+   */
+   	printf("Please enter the message: ");
+   	bzero(buffer,256);
+   	fgets(buffer,255,stdin);
+  	move(sockfd,buffer); 
 	//some boolean variables for different functionality within this
 	//program
 	bool trackObjects = true;
